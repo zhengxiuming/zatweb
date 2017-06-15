4fd4728605b512721fb6055435582ad3d65c4a4b
enorzw <enorzw@163.com>
2017-06-15 14:38:29 +0800

    mysql

--- php/application/controllers/Index.php
+++ php/application/controllers/Index.php
@@ -1,7 +1,16 @@
 <?php
+require_once APP_PATH.'/application/library/mysql/db_mysqli.php';
+require_once APP_PATH.'/application/library/mysql/db_funcs.php';
+
 class IndexController extends BaseController {
    public function indexAction() {//默认Action
-       $this->__errResponseJson(-1,'sfsdfs');
+       $mysql = new DbMysqli();
+       if($mysql){
+            $row = $mysql->getLine("select * from roles");
+            $res = json_encode($row);
+            $this->__responseJson($row);
+       }
+       
    }
 }
 ?>
