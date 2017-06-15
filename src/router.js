c4bbdb91745ed66da803af5314ea51e3f264bd5d
enorzw <enorzw@163.com>
2017-06-15 15:52:19 +0800

    antd

--- src/router.js
+++ src/router.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import HeaderComponent from './components/header';
 
 function RouterConfig({ history }) {
   return (
-    <Router history={history}>
-      <Route path="/" component={IndexPage} />
-    </Router>
+    <div>
+      <HeaderComponent />
+      <Router history={history}>
+        <Route path="/" component={IndexPage} />
+        <Route path="/a" component={HeaderComponent} />
+      </Router>
+    </div>
   );
 }
 
